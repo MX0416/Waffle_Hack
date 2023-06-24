939513741a59586b72bc707e2060cf1a6e446d2b
Matthew Xue <matthew.xue2004@gmail.com>
2023-06-24 14:08:25 -0700

    fixed API pull request

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import requests
 
 
-
-
-url = "http://api.openweathermap.org/data/2.5/air_pollution?lat={50}&lon={50}&appid={1d21e89e8a54cc7271a3bb5c38934787}"
-
+url = "https://api.openweathermap.org/data/2.5/forecast?lat=40.712776&lon=-74.005974&appid=1d21e89e8a54cc7271a3bb5c38934787"
 
 response = requests.get(url)
+
 json = response.json()
+
+# print(response.text)
+
 print(json)
