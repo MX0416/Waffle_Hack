4f2fac6727f1c21c318cf27cc13234fd9ee60a37
zlarsen23 <zachlarsen1050@gmail.com>
2023-06-24 17:00:40 -0500

    data assigned to variables

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import requests
+import json
 
 
-url = "https://api.openweathermap.org/data/2.5/forecast?lat=40.712776&lon=-74.005974&appid=1d21e89e8a54cc7271a3bb5c38934787"
+url = "https://api.openweathermap.org/data/2.5/weather?lat=44.977753&lon=-93.265015&units=imperial&appid=1d21e89e8a54cc7271a3bb5c38934787"
 
-response = requests.get(url)
+data = requests.get(url)
 
-json = response.json()
+result = data.json()
 
 # print(response.text)
 
-print(json)
+print(json.dumps(result, indent =4, sort_keys=True ))
+
+temp = result["main"]["temp"]
+humidity = result["main"]["humidity"]
+temp_max = result["main"]["temp_max"]
+temp_min = result["main"]["temp_min"]
+city_name = result["name"]
+description = result["weather"][0]["description"]
+feels_like = result["main"]["feels_like"]
+
+
+
+
+print(humidity)
