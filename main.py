b89e1b2a9399e4405b211c31f5619b62057ed525
zlarsen23 <zachlarsen1050@gmail.com>
2023-06-24 12:50:31 -0500

    API stuff

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-print("hello")
-print("111111")
-print("added third line")
+import requests
+
+
+
+
+url = "http://api.openweathermap.org/data/2.5/air_pollution?lat={50}&lon={50}&appid={1d21e89e8a54cc7271a3bb5c38934787}"
+
+
+response = requests.get(url)
+json = response.json()
+print(json)
